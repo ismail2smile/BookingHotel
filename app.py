58577aa40df4d3f7012b3fc5252e40f20b5f1951
ismail2smile <ismail2smile@gmail.com>
2023-09-20 20:44:04 +0530

    auth added  to secure the api

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from mongodb import initialize_mongodb  # Import MongoDB functions
 from flask_cors import CORS
-from auth import auth_bp  # Import the auth blueprint
+from auth import auth_bp, initialize_jwt  # Import the auth blueprint
 from hotel import Hotel
 from staff import Staff
 from booking import Booking
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+
 
 
 app = Flask(__name__)
@@ -13,11 +16,15 @@ app.register_blueprint(auth_bp, url_prefix="/auth")
 # MongoDB configuration
 initialize_mongodb(app)  # Initialize MongoDB connection
 
+# Initilize JWT
+initialize_jwt(app)
+
 # Set CORS headers for the entire Flask application
 CORS(app, origins=["http://localhost:3000"])
 
 
 # Endpoint to get a list of available hotels
+
 @app.route("/api/hotels", methods=["GET"])
 def get_hotels():
     #  data = request.get_json()
@@ -26,6 +33,7 @@ def get_hotels():
      return jsonify({"data":hotelList,"status":200})
 
 # Endpoint to book a hotel room
+@jwt_required()
 @app.route("/api/bookings", methods=["POST"])
 def book_hotel():
     data = request.get_json()
@@ -40,21 +48,29 @@ def book_hotel():
 
 
 # Endpoint to book a hotel room
-@app.route("/api/createHotel", methods=["POST"])
-def create_hotel():
-    data = request.get_json()
-    name = data.get("name")
-    description = data.get("description")
-    price_per_24Hrs = data.get("price_per_24Hrs")
-    location = data.get("location")
-    amenities = data.get("amenities")
-    contact_number = data.get("contact_number")
-    hotelID = data.get("hotelID")
-    contactname = data.get("contactname")
-    # Insert the booking data into MongoDB
-    result = Hotel.create_hotel(name,description,price_per_24Hrs,location,amenities,contactname,contact_number,hotelID)
 
-    return jsonify(result)
+@app.route("/api/createHotel", methods=["POST"])
+@jwt_required()
+def create_hotel():
+     try:
+        current_user = get_jwt_identity()
+        if not current_user:
+            return jsonify({"message": "Invalid token", "status": 401})
+        data = request.get_json()
+        name = data.get("name")
+        description = data.get("description")
+        price_per_24Hrs = data.get("price_per_24Hrs")
+        location = data.get("location")
+        amenities = data.get("amenities")
+        contact_number = data.get("contact_number")
+        hotelID = data.get("hotelID")
+        contactname = data.get("contactname")
+        # Insert the booking data into MongoDB
+        result = Hotel.create_hotel(name,description,price_per_24Hrs,location,amenities,contactname,contact_number,hotelID)
+
+        return jsonify(result)
+     except Exception as e:
+        return jsonify({"error": str(e), "status": 500})
 
 
 if __name__ == "__main__":
